26fa34bda4318baea3e093b173c55b3f0d9a5d0a
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-10-28 14:59:46 +0200

    added comments

--- flow.cpp
+++ flow.cpp
@@ -24,50 +24,77 @@ using namespace std;
 
 #define IP 2048
 
-// struct that represents the args
+/* struct that represents the args */
 typedef struct arguments {
-    char *file;
-    int active_timer;
-    int inactive_timer;
+    char *file;                     /* input file, is "-" (for STDIN) by default */
+    sockaddr_in netflow_collector;  /* IP address (possibly with port) of the NetFlow collector */
+    int active_timer;               /* time in seconds, after which active flows are exported, 60 by default */
+    int inactive_timer;             /* time in seconds, after which inactive flows are exported, 10 by default */
     int count; // TODO: count
-    sockaddr_in netflow_collector;
 } Arguments;
 
+/* struct representing the whole flow
+ * properties marked by "UNKNOWN" in the comment I set to 0 by default
+ * since we don't know, what their values should be
+ */
 typedef struct flow {
-    uint32_t srcAddr; /* source IP */
-    uint32_t dstAddr; /* destination IP */
-    uint32_t nextHop; /* IP address of next hop router - we don't know that */
-    uint16_t input; /* SNMP index of input interface - we don't know that */
-    uint16_t output; /* SNMP index of output interface - we don't know that */
-    uint32_t dPkts; /* amount of packets in the flow */
-    uint32_t dOctets; /* total number of Layer 3 bytes in the packets of the flow */
-    uint32_t first; /* when was the first packet sent */
-    uint32_t last; /* when was the last packet sent */
-    uint16_t srcPort; /* source port number */
-    uint16_t dstPort; /* destination port number */
-    uint8_t pad1; /* unused bytes - we don't know that */
-    uint8_t tcp_flags; /* TCP flags that appeared in the communication */
-    uint8_t protocol; /* IP protocol type */
-    uint8_t tos; /* IP type of service */
-    uint16_t src_as; /* we don't know that */
-    uint16_t dst_as; /* we don't know that */
-    uint8_t src_mask; /* we don't know that */
-    uint8_t dst_mask; /* we don't know that */
-    uint16_t pad2; /* we don't know that */
+    uint32_t srcAddr;       /* source IP */
+    uint32_t dstAddr;       /* destination IP */
+    uint32_t nextHop;       /* IP address of next hop router - UNKNOWN */
+    uint16_t input;         /* SNMP index of input interface - UNKNOWN */
+    uint16_t output;        /* SNMP index of output interface - UNKNOWN */
+    uint32_t dPkts;         /* amount of packets in the flow */
+    uint32_t dOctets;       /* total number of Layer 3 bytes in the packets of the flow */
+    uint32_t first;         /* when was the first packet sent */
+    uint32_t last;          /* when was the last packet sent */
+    uint16_t srcPort;       /* source port number */
+    uint16_t dstPort;       /* destination port number */
+    uint8_t pad1;           /* unused bytes - UNKNOWN */
+    uint8_t tcp_flags;      /* TCP flags that appeared in the communication */
+    uint8_t protocol;       /* IP protocol type */
+    uint8_t tos;            /* IP type of service */
+    uint16_t src_as;        /* UNKNOWN */
+    uint16_t dst_as;        /* UNKNOWN */
+    uint8_t src_mask;       /* UNKNOWN */
+    uint8_t dst_mask;       /* UNKNOWN */
+    uint16_t pad2;          /* UNKNOWN */
 } Flow;
 
+/* a key used for the flow_cache map, this is what is used for indexing
+ * the structure is as follows:
+ * - source IP address (uint32_t)
+ * - destination IP address (uint32_t)
+ * - source port (uint16_t)
+ * - destination port (uint16_t)
+ * - protocol (uint8_t)
+ * - type of service (uint8_t)
+ * */
 typedef tuple <uint32_t, uint32_t, uint16_t, uint16_t, uint8_t, uint8_t> Flow_key;
 
+/* global variables */
 map<Flow_key, Flow> flow_cache;
 typedef map<Flow_key, Flow>::iterator Iterator;
 uint32_t current_packet_time;
 Arguments arguments = {};
 
+/**
+ * When an error occurs, this function is called. It prints a message that is
+ * passed to it (it also prints a newline character so I don't need to pass it
+ * in the string all the time) and exits the app with a passed error code.
+ *
+ * @param message the message, that is printed to stderr
+ * @param code error code, with which the app terminates
+ * */
 void error_exit(const char *message, uint8_t code) {
     fprintf(stderr, "%s\n", message);
     exit(code);
 }
 
+/**
+ * Send a flow to the NetFlow collector specified in the arguments (or the default one).
+ *
+ * @param flow The flow to be sent
+ * */
 void export_flow(Flow flow) {
     int sock, i;
 
@@ -82,6 +109,12 @@ void export_flow(Flow flow) {
     else if (i != sizeof (Flow)) error_exit("send(): buffer written partially", 1);
 }
 
+/**
+ * Goes through every single flow in the flow cache and checks its timers.
+ * If the flow is active for more than the amount of seconds specified
+ * in the active_timer argument or if it had been inactive for more than
+ * the amount of seconds specified in the inactive_timer, it is exported.
+ * */
 void check_flow_timers() {
     // deleting according to this stack overflow site -> https://stackoverflow.com/questions/8234779/how-to-remove-from-a-map-while-iterating-it
     for (Iterator it = flow_cache.begin(); it != flow_cache.end(); /* empty on purpose */) {
@@ -94,6 +127,15 @@ void check_flow_timers() {
     }
 }
 
+/**
+ * For each packet this function is called. It attempts to find the according
+ * flow in the flow cache. If it doesn't find it, new flow is created and
+ * inserted to the flow cache. Else it updates the corresponding flow.
+ *
+ * @param key used for searching and indexing in the flow cache
+ * @param tcp_flags tcp_flags exported from the packet earlier
+ * @param dOctets dOctets are calculated earlier from the ether_header
+ * */
 void handle_flow(Flow_key key, uint8_t tcp_flags, uint32_t dOctets) {
     check_flow_timers();
 
@@ -117,12 +159,21 @@ void handle_flow(Flow_key key, uint8_t tcp_flags, uint32_t dOctets) {
     }
 }
 
+/**
+ * Creates a flow key for the ICMP packet
+ * */
 void icmp_packet(struct ip *iph, uint16_t protocol, uint32_t dOctets) {
     Flow_key key(iph->ip_src.s_addr, iph->ip_dst.s_addr, 0, 0, protocol, iph->ip_tos);
     handle_flow(key, 0, dOctets);
 }
 
-/* header size is the ip header size + the ethernet header size */
+/**
+ * Creates a flow key for the TCP packet
+ *
+ * @param bytes the packet
+ * @param iph IP header
+ * @param header_size the ip header size + the ethernet header size
+ * */
 void tcp_packet(const u_char *bytes, struct ip *iph, u_int header_size, uint32_t dOctets) {
     struct tcphdr *tcph = (struct tcphdr*)(bytes + header_size);
 
@@ -130,6 +181,13 @@ void tcp_packet(const u_char *bytes, struct ip *iph, u_int header_size, uint32_t
     handle_flow(key, tcph->th_flags, dOctets);
 }
 
+/**
+ * Creates a flow key for the UDP packet
+ *
+ * @param bytes the packet
+ * @param iph IP header
+ * @param header_size the ip header size + the ethernet header size
+ * */
 void udp_packet(const u_char *bytes, struct ip *iph, u_int header_size, uint32_t dOctets) {
     struct udphdr *udph = (struct udphdr *)(bytes + header_size);
 
@@ -137,6 +195,12 @@ void udp_packet(const u_char *bytes, struct ip *iph, u_int header_size, uint32_t
     handle_flow(key, 0, dOctets);
 }
 
+/**
+ * Finds out the packet type (ICMP/UDP/TCP) and calls the corresponding function.
+ *
+ * @param bytes the packet
+ * @param dOctets dOctets calculated earlier from the packet header - only to pass it forward
+ * */
 void handle_ipv4(const u_char *bytes, uint32_t dOctets) {
     // the ip structure is described here -> https://sites.uclouvain.be/SystInfo/usr/include/netinet/ip.h.html
     struct ip *iph = (struct ip*)(bytes + sizeof(struct ether_header));
@@ -157,6 +221,17 @@ void handle_ipv4(const u_char *bytes, uint32_t dOctets) {
     }
 }
 
+/**
+ * The pcap_loop function calls this function for every packet from the input.
+ * Finds out the time of the packet and saves it to a global variable. Then calculates
+ * dOctets (caplen from packet header - the size of ether_header). Finally calls
+ * the next function only if the packet is IPv4. I do not support NetFlow v9, so I
+ * do not have function for IPv6.
+ *
+ * @param user unused parameter, it is passed from the pcap_loop function, but I don't need it
+ * @param h the packet header
+ * @param bytes the packet itself
+ * */
 void callback (u_char *user __attribute__((unused)), const struct pcap_pkthdr *h, const u_char *bytes) {
     current_packet_time = h->ts.tv_sec*1000 + h->ts.tv_usec/1000;
     struct ether_header *header = (struct ether_header *) bytes;
@@ -166,6 +241,11 @@ void callback (u_char *user __attribute__((unused)), const struct pcap_pkthdr *h
     if (ntohs(header->ether_type) == IP) handle_ipv4(bytes, dOctets);
 }
 
+/**
+ * Returns the default netflow collector specified in the task (127.0.0.1:2055).
+ *
+ * @return default netflow collector
+ * */
 sockaddr_in get_default_netflow_collector() {
     sockaddr_in default_netflow_collector;
     struct hostent *servent;
@@ -182,6 +262,12 @@ sockaddr_in get_default_netflow_collector() {
     return default_netflow_collector;
 }
 
+/**
+ * Using getopt library parses arguments and stores them in the global struct for arguments.
+ *
+ * @param argc amount of arguments
+ * @param argv array containing the arguments
+ * */
 void parse_arguments(int argc, char **argv) {
     // working with getopt library studied from here
     // https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Option-Example.html
@@ -251,6 +337,11 @@ void parse_arguments(int argc, char **argv) {
     }
 }
 
+/**
+ * The primary function working mainly with the pcap library.
+ *
+ * @return the error code
+ * */
 int export_flows_from_pcap_file() {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_if_t *alldevsp;
@@ -295,7 +386,7 @@ int export_flows_from_pcap_file() {
 
 int main(int argc, char **argv) {
     char stdin_selector[] = "-";
-    arguments = { stdin_selector, 60, 10, 1024, get_default_netflow_collector() };
+    arguments = { stdin_selector, get_default_netflow_collector(), 60, 10, 1024 };
     parse_arguments(argc, argv);
 
     return export_flows_from_pcap_file();
